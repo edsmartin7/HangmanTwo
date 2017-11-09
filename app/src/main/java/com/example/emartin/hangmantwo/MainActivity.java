905306485b4a99f2877c1d74fd66e8b209617c43
edsmartin7 <edsmartin7@gmail.com>
2017-11-08 23:56:16 -0500

    first commit

---
+++ app/src/main/java/com/example/emartin/hangmantwo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.emartin.hangmantwo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+//https://developer.android.com/guide/topics/resources/drawable-resource.html
+//https://developer.android.com/reference/android/graphics/drawable/ShapeDrawable.html
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen_layout);
+
+    }
+
+    public void startSinglePlayerGame(View view) {
+
+        //create an intent, to start a new activity
+        Intent myIntent = new Intent(this, GameActivity.class);
+        startActivity(myIntent);
+
+    }
+
+
+}
